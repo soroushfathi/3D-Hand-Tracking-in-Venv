baa3e179c571aebca00dd1a6436dac810b5ae397
soroushfathi <81929775+soroushfathi@users.noreply.github.com>
2022-07-20 00:53:28 +0430

    unity assets

---
+++ assests (c#)/HandTracking.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandTracking : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public UDPReceive updReceive;
+    public GameObject[] handPoints;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string data = updReceive.data;
+        data = data.Remove(0, 1);
+        data = data.Remove(data.Length-1, 1);
+        string[] points = data.Split(',');
+
+        for (int i=0; i<21; i++)
+        {
+            float x = float.Parse(points[i*3]) / 100 - 10;
+            float y = float.Parse(points[i*3 + 1]) / 100 - 5;
+            float z = float.Parse(points[i*3 + 2]) / 100;
+            handPoints[i].transform.localPosition = new Vector3(x, y, z);
+        }
+        if (data.Length >= 21) 
+        {
+            for (int i=21; i<42; i++)
+            {
+                float x = float.Parse(points[i*3]) / 100 - 10;
+                float y = float.Parse(points[i*3 + 1]) / 100 - 5;
+                float z = float.Parse(points[i*3 + 2]) / 100;
+                handPoints[i].transform.localPosition = new Vector3(x, y, z);
+            }
+        } 
+    }
+}
