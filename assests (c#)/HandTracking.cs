1b4a1d0a8b744d2755ee0db21159a8e705e64872
soroushfathi <81929775+soroushfathi@users.noreply.github.com>
2022-07-25 12:21:54 +0430

    improve handtracking func(c#) for two hands

--- assests (c#)/HandTracking.cs
+++ assests (c#)/HandTracking.cs
@@ -20,20 +20,31 @@ public class HandTracking : MonoBehaviour
         data = data.Remove(data.Length-1, 1);
         string[] points = data.Split(',');
 
-        for (int i=0; i<21; i++)
-        {
-            float x = float.Parse(points[i*3]) / 100 - 10;
-            float y = float.Parse(points[i*3 + 1]) / 100 - 5;
-            float z = float.Parse(points[i*3 + 2]) / 100;
-            handPoints[i].transform.localPosition = new Vector3(x, y, z);
-        }
-        if (data.Length >= 21) 
-        {
-            for (int i=21; i<42; i++)
-            {
+        if (data.Length == 0) {
+            for (int i=0; i<42; i++) {
+                handPoints[i].transform.localPosition = new Vector3(0, 0, 0);
+            }
+        } else if (data.Length == 21) {
+            for (int i=0; i<21; i++) {
                 float x = float.Parse(points[i*3]) / 100 - 10;
                 float y = float.Parse(points[i*3 + 1]) / 100 - 5;
-                float z = float.Parse(points[i*3 + 2]) / 100;
+                float z = - float.Parse(points[i*3 + 2]) / 4 + 10;
+                handPoints[i].transform.localPosition = new Vector3(x, y, z);
+            }
+            for (int i=21; i<42; i++) {
+                handPoints[i].transform.localPosition = new Vector3(0, 0, 0);
+            }
+        } else if (data.Length > 21) {
+            for (int i=0; i<21; i++) {
+                float x = float.Parse(points[i*3]) / 100 - 10;
+                float y = float.Parse(points[i*3 + 1]) / 100 - 5;
+                float z = - float.Parse(points[i*3 + 2]) / 4 + 10;
+                handPoints[i].transform.localPosition = new Vector3(x, y, z);
+            }
+            for (int i=21; i<42; i++) {
+                float x = float.Parse(points[i*3]) / 100 - 10;
+                float y = float.Parse(points[i*3 + 1]) / 100 - 5;
+                float z = - float.Parse(points[i*3 + 2]) / 4 + 10;
                 handPoints[i].transform.localPosition = new Vector3(x, y, z);
             }
         } 
